e970e94054732e9e10ecd7cf4e59cdbe471ae046
Brian Wu <91295015+briwu@users.noreply.github.com>
2023-11-05 23:06:31 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,58 @@
+var video;
+
+window.addEventListener("load", function () {
+	console.log("Good job opening the window");
+	video = document.querySelector(".video");
+	video.autoplay = false;
+	video.loop = false;
+	video.volume = 1;
+	video.muted = false;
+
+	document.querySelector("#play").addEventListener("click", function () {
+		console.log("Play Video");
+		video.play();
+		video.volume = 1;
+		console.log((video.volume * 100) + "%");
+	});
+
+	document.querySelector("#pause").addEventListener("click", function () {
+		video.pause();
+	});
+
+	document.querySelector("#slower").addEventListener("click", function () {
+		video.playbackRate -= 0.1;
+		console.log((video.playbackRate * 100).toFixed(0));
+	});
+
+	document.querySelector("#faster").addEventListener("click", function () {
+		video.playbackRate += 0.1;
+		speed += 0.1;
+		console.log((video.playbackRate * 100).toFixed(0));
+	});
+
+	document.querySelector("#skip").addEventListener("click", function () {
+		video.currentTime += 10;
+		if (video.currentTime > video.duration) {
+			video.currentTime = 0;
+		}
+		console.log(video.currentTime.toFixed(2));
+	});
+
+	document.querySelector("#mute").addEventListener("click", function () {
+		video.muted = !video.muted;
+		document.querySelector("#mute").textContent = video.muted ? "Unmute" : "Mute";
+	});
+
+	document.querySelector("#slider").addEventListener("input", function () {
+		video.volume = this.value / 100;
+		console.log(video.volume * 100 + "%");
+	});
+
+	document.querySelector("#vintage").addEventListener("click", function () {
+		video.classList.add("oldSchool");
+	});
+
+	document.querySelector("#orig").addEventListener("click", function () {
+		video.classList.remove("oldSchool");
+	});
+});
